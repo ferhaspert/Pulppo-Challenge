6af735fe98aeaf935884c28048b1d523f50d5a82
Fernando Haspert <fernando.haspert@kavak.com>
2024-12-23 18:09:09 -0300

    add a graph to show the property prices by state

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,29 @@
 'use client';
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
-import { IProperty } from '@/models/property.model';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 const Dashboard = () => {
-  const [properties, setProperties] = useState<IProperty[]>([]);
+  const [pricesByState, setPricesByState] = useState([]);
 
   useEffect(() => {
     const fetchProperties = async () => {
       try {
-        const response = await fetch('/api/property');
-        const data: IProperty[] = await response.json();
-        console.log(`setting data: ${JSON.stringify(data)}`);
-        setProperties(data);
+        const response = await fetch('/api/state');
+        const data = await response.json();
+        setPricesByState(data);
       } catch (error) {
-        console.error('Error fetching properties:', error);
+        console.error('Error fetching prices:', error);
       }
     };
 
@@ -28,21 +36,54 @@ const Dashboard = () => {
       <main className="max-w-4xl mx-auto p-4">
         <section className="my-8 p-4">
           <h2 className="text-2xl font-bold mb-4">Gráficos</h2>
-          <p>cantidad de propiedades: {properties.length}</p>
-          {properties.length > 0 ? (
-            <ul>
-              {properties.map((property, index) => (
-                <li key={index}>
-                  <p>{property.address?.state?.name}</p>
-                </li>
-              ))}
-            </ul>
+          {pricesByState.length ? (
+            <PricesByStateGraph data={pricesByState} />
           ) : (
-            <div>Loading...</div>
+            <p>Loading...</p>
           )}
         </section>
       </main>
       <Footer />
+    </div>
+  );
+};
+
+const PricesByStateGraph = ({ data }: { data: any }) => {
+  return (
+    <div>
+      <h1>Precios por Estado</h1>
+      <ResponsiveContainer
+        minWidth={'500px'}
+        minHeight={'300px'}
+        width="100%"
+        height="100%"
+      >
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="min"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="max" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="avg" stroke="#ca8282" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
